a1d4f7f292df7466abf6055f44b29b19ba4ae8a8
Zack Maril <zack@zacharymaril.com>
2013-03-10 13:47:55 +0400

    Update to not just work on my machine.

--- test/hermes/persistent/conf.clj
+++ test/hermes/persistent/conf.clj
@@ -5,8 +5,9 @@
                      :hostname "127.0.0.1"
                      :keyspace "hermestest"
                      :cassandra-config-dir
-                     ;;TODO: make this work from root of hermes
-                     "file:///Users/zackmaril/Projects/aurelius-stack/hermes/resources/test-cassandra.yaml"}})
+                     (str "file://"
+                          (System/getProperty "user.dir")
+                          "/resources/test-cassandra.yaml")}})
 
 (defn clear-db []
   (FileUtils/deleteDirectory (java.io.File. "/tmp/hermes-test")))
